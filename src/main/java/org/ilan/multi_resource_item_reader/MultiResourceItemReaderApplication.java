06da81f9e1d37f5959d50bd9afbbd4cc07c5d558
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2024-12-17 13:08:45 +0000

    first commit

---
+++ src/main/java/org/ilan/multi_resource_item_reader/MultiResourceItemReaderApplication.java
@@ -0,0 +1,13 @@
+package org.ilan.multi_resource_item_reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiResourceItemReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiResourceItemReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ilan/multi_resource_item_reader/MultiResourceItemReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ilan.multi_resource_item_reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiResourceItemReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
